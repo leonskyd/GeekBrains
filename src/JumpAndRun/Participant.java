bd381ce3aacf297eec9c3cefd234719f50ec95f1
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2020-11-15 16:11:50 +0200

    Update Participant.java

--- src/JumpAndRun/Participant.java
+++ src/JumpAndRun/Participant.java
@@ -12,20 +12,22 @@ public class Participant {
         this.jump = jump;
         this.run = run;}
 
-    public void runA(double distance) {
+    public boolean runA(double distance) {
         if (run >= distance) {
             System.out.println(name + " is running !");
         } else {
-            System.out.println(name + " cannot run this distance, the race is over !");
+            System.out.println(name + " cannot run this distance !");
         }
+        return run >= disyance;
     }
 
-    public void jumpA(double height) {
+    public boolean jumpA(double height) {
         if (jump >= height) {
             System.out.println(name + " is jumping over !");
         } else {
-            System.out.println(name + " cannot jump over this height, the race is over !");
+            System.out.println(name + " cannot jump over this height !");
         }
+        return jump >= height;
     }
 }
 
